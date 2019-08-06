87f8837837e56507ffb610c7e0fc61f5ecf35422
Jose Perales <josepablopt@gmail.com>
2019-08-05 20:52:06 -0500

    added card

--- src/pages/landingpage.js
+++ src/pages/landingpage.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react'
 import HideAppBar from '../components/header/bar'
+import HeadingPicture from '../components/landing/SectionCarousel'
+import { Container } from '@material-ui/core'
+import ImgMediaCard from '../components/body/Card'
+import { makeStyles } from '@material-ui/core/styles';
+import FullWidthGrid from '../components/body/Full'
+
 
 export default class LandingPage extends Component {
+    
     render() {
+        const divStyle = {
+            padding: "10px",
+            margin: "10px"
+          
+          };
+      
+
       return (
         <div>
+    
+
             <HideAppBar></HideAppBar>
-            <h1>hello</h1>
+            <HeadingPicture></HeadingPicture>
+            <FullWidthGrid> </FullWidthGrid>
+       
+  
+  
+            
+       
+            
+           
+            
+            
         </div>
       )
     }
   }
+
